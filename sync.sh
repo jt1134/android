5b3371fbe0ece1eac146a5a6a1af1d9e970f8bfd
jt1134 <jt1134@gmail.com>
2014-08-30 20:17:40 -0700

    wwwwwwwwwwwwaaaaaaaaaaaaaaaaaaaa

--- sync.sh
+++ sync.sh
@@ -7,20 +7,11 @@ TRY() { "$@" || DIE "$@"; }
 # just in case
 TRY cd $(dirname $0)"/../"
 
-which repo > /dev/null
-if [ $? != 0 ]; then
-  mkdir -p /usr/local/bin
-  curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > /usr/local/bin/repo
-  chmod a+x /usr/local/bin/repo
-  # we're root (should be)
-  exit
-fi
-
 if [ ! -d .repo/local_manifests ]; then
   TRY repo init -u git://github.com/CyanogenMod/android.git -b cm-11.0
   TRY mkdir -p .repo/local_manifests
 fi
 
 TRY cp android/local_manifest.xml .repo/local_manifests/roomservice.xml
-TRY repo sync -j1
+TRY repo sync -j40
 
